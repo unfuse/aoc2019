d79ecc6d12d78344cbb48484ff790f0ff4683b5e
David Kennedy <david.kennedy@workday.com>
2019-12-14 23:25:59 -0800

    notes on day part usage

--- day5.py
+++ day5.py
@@ -3,6 +3,8 @@ import csv, intcodecomputer
 with open("input5.txt") as csvfile:
     data = list(csv.reader(csvfile))[0]
 
+
+# 1 for 5.1, and 5 for 5.2
 intcodecomputer.defaultComputer(data).run()
 
 
